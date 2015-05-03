bfd3bdfb047047475bd9d471f9ae30b4b6a2b40b
Forrest Thomas <forrestthomas@forrests-mbp-2.attlocal.net>
2015-05-02 18:31:36 -0700

    use express server for index.html

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 var watch = require('gulp-watch');
+var express = require('express');
+var app = express();
+var path = require('path');
 
 gulp.task('build', function() {
 	browserify(['./src/index.jsx'])
@@ -12,8 +15,18 @@ gulp.task('build', function() {
 		.pipe(gulp.dest('./'));
 });
 
+gulp.task('serve', function() {
+	app.get('/', function(request, response) {
+		response.sendFile(path.join(__dirname + '/index.html'));
+	});
+	app.get('/bundle', function(request, response) {
+		response.sendFile(path.join(__dirname + '/bundle.js'));
+	})
+	app.listen(3000);
+})
+
 gulp.task('watch', function() {
 	gulp.watch(['./src/**/*.jsx'], ['build'])
 })
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['build', 'serve', 'watch']);
